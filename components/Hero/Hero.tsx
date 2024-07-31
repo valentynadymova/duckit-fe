c354b3e0e3a7ea3fde47632974729bb46bb83b6f
andriiKrakovskyi <152904650+andriiKrakovskyi@users.noreply.github.com>
2024-07-31 13:19:38 +0200

    -add-styles-to header

--- components/Hero/Hero.tsx
+++ components/Hero/Hero.tsx
@@ -13,11 +13,30 @@ const Hero = () => {
           for Your Success
           <span className={styles.hero_stop}>.</span>
         </h1>
-        <p className={styles.hero_subtitle}>
+        <p className={styles.hero_text}>
           We specialize in creating unique websites and mobile applications
           tailored to your business needs
         </p>
         <button className={styles.hero_button}>Get Started</button>
+
+        <p className={`${styles.aboutus_subtitle} subtitle`}>// about us</p>
+
+        <h2 className={styles.aboutus_title}>Our Proven Track Record</h2>
+
+        <ul className={styles.aboutus_list}>
+          <li className={styles.aboutus_item}>
+            <p className={styles.aboutus_span}>10+</p>
+            <h5 className={styles.aboutus_item_title}>Years of Experience</h5>
+          </li>
+          <li className={styles.aboutus_item}>
+            <p className={styles.aboutus_span}>100+</p>
+            <h5 className={styles.aboutus_item_title}>Projects</h5>
+          </li>
+          <li className={styles.aboutus_item}>
+            <p className={styles.aboutus_span}>99%</p>
+            <h5 className={styles.aboutus_item_title}>Customer Satisfaction</h5>
+          </li>
+        </ul>
       </Container>
     </section>
   );
