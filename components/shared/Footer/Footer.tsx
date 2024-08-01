ff9d1359626d8e26303cf272dfe47267d75935ac
andriiKrakovskyi <152904650+andriiKrakovskyi@users.noreply.github.com>
2024-08-01 21:37:31 +0200

    Mobile media (#5)
    * draft
    
    * contactus
    
    * form-contactus
    
    * footer-teg
    
    ---------
    
    Co-authored-by: ValentynaDymova <83668145+valentynadymova@users.noreply.github.com>

--- components/shared/Footer/Footer.tsx
+++ components/shared/Footer/Footer.tsx
@@ -7,13 +7,63 @@ import '@/styles/reset.css';
 const Footer = () => {
   return (
     <footer className={styles.footer_section}>
-      <div className={`${styles.footer_container} ${styles.container}`}>
-        <svg>
-          <use href=""></use>
-        </svg>
-        <p></p>
-        <link></link>
-      </div>
+      <Container className={styles.footer_container}>
+        <div className={styles.footer_wrap_general}>
+          <div className={styles.footer_wrap_logo}>
+            <svg className={styles.footer_logo} width="106" height="62">
+              <use href=""></use>
+            </svg>
+            <p>Web and Mobile Solutions for Your Success</p>
+            <a>Privacy policy</a>
+          </div>
+          <nav className={styles.footer_navigation}>
+            <p>Navigation</p>
+            <ul>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+              <li></li>
+            </ul>
+          </nav>
+          <address className={styles.footer_adrress}></address>
+          <div className={styles.footer_wrap_social}>
+            <p>Social media</p>
+            <ul>
+              <li>
+                <svg
+                  className={styles.footer_social_logo}
+                  width="32"
+                  height="32"
+                >
+                  <use href=""></use>
+                </svg>
+              </li>
+              <li>
+                <svg
+                  className={styles.footer_social_logo}
+                  width="32"
+                  height="32"
+                >
+                  <use href=""></use>
+                </svg>
+              </li>
+              <li>
+                <svg
+                  className={styles.footer_social_logo}
+                  width="32"
+                  height="32"
+                >
+                  <use href=""></use>
+                </svg>
+              </li>
+            </ul>
+          </div>
+          <p>
+            <span>&#169;</span>2024 Duck_it. All rights reserved.
+          </p>
+        </div>
+      </Container>
     </footer>
   );
 };
