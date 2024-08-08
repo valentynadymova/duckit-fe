5ceea55b1aa3b50f14cb39467b7e2c5bd3ab400a
andriiKrakovskyi <152904650+andriiKrakovskyi@users.noreply.github.com>
2024-08-08 20:46:14 +0200

    ourfeatures-img-tailored-list

--- components/OurFeatures/OurFeatures.tsx
+++ components/OurFeatures/OurFeatures.tsx
@@ -20,52 +20,6 @@ const OurFeatures = () => {
               business&#46; Easily handle increased traffic and add new features
               without compromising performance&#46;
             </p>
-            <picture>
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/webp"
-                media="(min-width: 1440px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/png"
-                media="(min-width: 1440px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/webp"
-                media="(min-width: 768px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/png"
-                media="(min-width: 768px)"
-              />
-              <source
-                srcSet="/images/img-320px/scalability-320px-1x.webp 1x,
-                    /images/img-320px/scalability-320px-2x.webp 2x"
-                type="image/webp"
-                media="(min-width: 360px)"
-              />
-              <source
-                srcSet="/images/img-320px/scalability-320px-1x.png 1x,
-                    /images/img-320px/scalability-320px-2x.png 2x"
-                type="image/png"
-                media="(min-width: 360px)"
-              />
-              <img
-                src=""
-                alt=""
-                width=""
-                height=""
-                className={styles.ourfeatures_img}
-                loading="lazy"
-              />
-            </picture>
           </li>
           <li className={styles.ourfeatures_item}>
             <h3 className={styles.ourfeatures_item_title}>
@@ -115,53 +69,8 @@ const OurFeatures = () => {
               innovation&#44; performance&#44; security&#44; and
               scalability&#46;
             </p>
-            <picture>
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/webp"
-                media="(min-width: 1440px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/png"
-                media="(min-width: 1440px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/webp"
-                media="(min-width: 768px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/png"
-                media="(min-width: 768px)"
-              />
-              <source
-                srcSet="/images/img-320px/comprehensive-320px-1x.webp 1x,
-                    /images/img-320px/comprehensive-320px-2x.webp 2x"
-                type="image/webp"
-                media="(min-width: 360px)"
-              />
-              <source
-                srcSet="/images/img-320px/comprehensive-320px-1x.png 1x,
-                    /images/img-320px/comprehensive-320px-2x.png 2x"
-                type="image/png"
-                media="(min-width: 360px)"
-              />
-              <img
-                src=""
-                alt=""
-                width=""
-                height=""
-                className={styles.ourfeatures_img}
-                loading="lazy"
-              />
-            </picture>
           </li>
+
           <li className={styles.ourfeatures_item}>
             <h3 className={styles.ourfeatures_item_title}>
               Tailored solutions
@@ -171,56 +80,12 @@ const OurFeatures = () => {
               each client&#8217;s unique needs and goals&#44; ensuring
               satisfaction and success&#46;
             </p>
-            {/* <picture>
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/webp"
-                media="(min-width: 1440px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/png"
-                media="(min-width: 1440px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/webp"
-                media="(min-width: 768px)"
-              />
-              <source
-                srcSet=" 1x,
-                     2x"
-                type="image/png"
-                media="(min-width: 768px)"
-              />
-              <source
-                srcSet="/images/img-320px/tailored-320px-1x.webp 1x,
-                    /images/img-320px/tailored-320px-2x.webp 2x"
-                type="image/webp"
-                media="(min-width: 360px)"
-              />
-              <source
-                srcSet="/images/img-320px/tailored-320px-1x.png 1x,
-                    /images/img-320px/tailored-320px-2x.png 2x"
-                type="image/png"
-                media="(min-width: 360px)"
-              />
-              <img
-                src=""
-                alt=""
-                width=""
-                height=""
-                className={styles.ourfeatures_img}
-                loading="lazy"
-              />
-            </picture> */}
+
             <div className={styles.ourfeatures_wrap_img_tailored_general}>
               <div className={styles.ourfeatures_wrap_img_tailored}>
                 <ul className={styles.ourfeatures_img_tailored_list}>
-                  <li className={styles.ourfeatures_people_item}>
+
+                  <li className={styles.ourfeatures_item_people}>
                     <Image
                       className={styles.ourfeatures_img_people}
                       alt="people"
@@ -232,7 +97,8 @@ const OurFeatures = () => {
                       Duck_IT team
                     </p>
                   </li>
-                  <li></li>
+
+                  <li className={styles.ourfeatures_item_arrow_left}>
                   <Image
                     className={styles.ourfeatures_img_arrow_left}
                     alt="arrow-left"
@@ -240,8 +106,9 @@ const OurFeatures = () => {
                     height={32}
                     src="/images/arrow-left.png"
                   />
-                  <li></li>
-                  <div className={styles.ourfeatures_wrap_lamp}>
+                  </li>
+
+                  <li  className={styles.ourfeatures_item_lamp}>
                     <Image
                       className={styles.ourfeatures_img_lamp}
                       alt="lamp-on"
@@ -252,17 +119,19 @@ const OurFeatures = () => {
                     <p className={styles.ourfeatures_text_lamp}>
                       Customized_solutions
                     </p>
-                  </div>
-                  <li></li>
+                  </li>
+                  
+                  <li className={styles.ourfeatures_item_arrow_right}>
                   <Image
                     className={styles.ourfeatures_img_arrow_right}
                     alt="arrow-right"
                     width={32}
                     height={32}
                     src="/images/arrow-right.png"
-                  />
-                  <li></li>
-                  <div className={styles.ourfeatures_wrap_profile}>
+                    />
+                  </li>
+                  
+                  <li className={styles.ourfeatures_item_profile}>
                     <Image
                       className={styles.ourfeatures_img_profile}
                       alt="profile"
@@ -271,7 +140,8 @@ const OurFeatures = () => {
                       src="/images/profile.png"
                     />
                     <p className={styles.ourfeatures_text_profile}>Customer</p>
-                  </div>
+                  </li>
+                  
                 </ul>
               </div>
             </div>
